d4ada7afc92035de16028927563699b11dc34101
Ubuntu <ubuntu@ip-172-31-21-204.ec2.internal>
2021-09-10 16:49:01 +0000

    added new well formated hello_world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Liu
+# Created on: Sept 2021
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
